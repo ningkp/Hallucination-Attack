471b0cf9a9420d8b4e812bc25a34109cb71e455b
ningkp <ningkp@nuaa.edu.cn>
2023-11-02 21:18:32 +0800

    init LLME

---
+++ LLME.py
@@ -0,0 +1,143 @@
+import gym
+from gym import spaces
+import numpy as np
+import random
+from config import ModelConfig
+from torch.nn.functional import cross_entropy
+from utils import load_model_and_tokenizer, complete_input, extract_model_embedding, random_init
+
+class LLMEnvironment(gym.Env):
+    def __init__(self, model_name, target_text, length=20, device='cuda:7'):
+        super(LLMEnvironment, self).__init__()
+        self.device = device
+
+        try:
+            self.model_config = getattr(ModelConfig, model_name)[0]
+        except AttributeError:
+            raise NotImplementedError
+
+        self.model, self.tokenizer = load_model_and_tokenizer(
+            self.model_config['path'], self.device, False
+        )
+
+        # 定义状态空间为tokenizer的空间
+        self.initial_text = random_init(model_name, length=length)
+        self.state_space = list(range(len(self.tokenizer.get_vocab())))
+        self.action_space = spaces.Tuple((spaces.Discrete(length), spaces.Discrete(len(self.tokenizer.get_vocab()))))
+        self.model_name = model_name
+
+        # 初始化环境
+        self.current_state = self.initial_text
+        self.current_state_ids = self.tokenizer(
+            self.current_state, truncation=True, return_tensors='pt'
+        ).input_ids[0].to(self.device)
+        self.target_state = target_text
+        self.current_loss = self.calculate_loss()
+
+    def reset(self):
+        # 重置环境为初始状态
+        self.current_state = self.initial_text
+        self.current_state_ids = self.tokenizer(
+            self.current_state, truncation=True, return_tensors='pt'
+        ).input_ids[0].to(self.device)
+        self.current_loss = self.calculate_loss()
+        return self.current_state, self.current_state_ids
+
+    def step(self, action):
+        # 执行动作：替换token
+        position, new_token_index = action
+        self.current_state_ids[position+1] = self.state_space[new_token_index]  # 起始位置不能替换
+        self.current_state = self.tokenizer.decode(
+            self.current_state_ids,
+            skip_special_tokens=True,
+        )
+
+        # 计算新的损失
+        new_loss = self.calculate_loss()
+
+        # 计算奖励为损失变化
+        reward = self.current_loss - new_loss
+        self.current_loss = new_loss
+
+        # 判断是否达到目标状态
+        done = self.current_state == self.target_state
+
+        # 返回新状态、奖励、是否结束和其他信息
+        return self.current_state, reward, done, {}
+
+    def calculate_loss(self):
+        # 切片
+        self.slice()
+        # 根据 current_state 和 target_state 来计算loss
+        input_str = complete_input(self.model_config, self.current_state)
+        if input_str.endswith(':'):
+            input_str += ' '
+        input_str += self.target_state
+        input_ids = self.tokenizer(
+            input_str, truncation=True, return_tensors='pt'
+        ).input_ids[0].to(self.device)
+
+        logits = self.model(input_ids=input_ids.unsqueeze(0))[0]
+        if logits.dim() > 2:
+            logits = logits.squeeze()
+        try:
+            assert input_ids.shape[0] >= self.target_slice.stop
+        except AssertionError:
+            self.target_slice = slice(self.target_slice.start, input_ids.shape[0])
+
+        compute_logits = logits[self.target_slice.start - 1: self.target_slice.stop - 1]
+        target = input_ids[self.target_slice]
+        loss = cross_entropy(compute_logits, target)
+        return loss
+
+    def slice(self):
+        prefix = self.model_config.get('prefix', '')
+        prompt = self.model_config.get('prompt', '')
+        suffix = self.model_config.get('suffix', '')
+        temp_str = prefix+prompt
+        temp_tokens = self.tokenizer(temp_str).input_ids
+        len1 = len(temp_tokens)
+        temp_str += self.current_state
+        temp_tokens = self.tokenizer(temp_str).input_ids
+        self.input_slice = slice(len1, len(temp_tokens))
+        try:
+            assert self.tokenizer.decode(temp_tokens[self.input_slice]) == self.current_state
+        except AssertionError:
+            self.input_slice = slice(self.input_slice.start-1, self.input_slice.stop)
+            try:
+                assert self.tokenizer.decode(temp_tokens[self.input_slice]) == self.current_state
+            except AssertionError:
+                if self.tokenizer.decode(temp_tokens[self.input_slice]).lstrip() != self.current_state:
+                    ### Todo
+                    raise NotImplementedError
+
+        temp_str += suffix
+        temp_tokens = self.tokenizer(temp_str).input_ids
+        len2 = len(temp_tokens)
+        if suffix.endswith(':'):
+            temp_str += ' '
+        temp_str += self.target_state
+        temp_tokens = self.tokenizer(temp_str).input_ids
+        self.target_slice = slice(len2, len(temp_tokens))
+
+
+
+
+if __name__ == '__main__':
+    target_text = 'This is an example text'
+    model_name = 'vicuna'
+    env = LLMEnvironment(model_name, target_text)
+
+    # 重置环境
+    state, state_ids = env.reset()
+    print("初始状态:", state)
+
+    # 执行两个动作
+    action1 = (2, 6)  # 假设你要替换第三个token为第七个token
+    action2 = (4, 1)  # 假设你要替换第五个token为第二个token
+    state, reward, done, _ = env.step(action1)
+    print("新状态:", state)
+    print("奖励:", reward)
+    state, reward, done, _ = env.step(action2)
+    print("新状态:", state)
+    print("奖励:", reward)
--- main.py
+++ main.py
@@ -1,38 +1,109 @@
-from attacker import Attacker
-from utils import random_init
+import gym
+import torch
+import argparse
+import numpy as np
+import torch.optim as optim
+from RL.model import Actor, Critic
+from RL.utils import get_action
+from collections import deque
+from RL.running_state import ZFilter
+from RL.hparams import HyperParams as hp
+import matplotlib.pyplot as plt
+from LLME import LLMEnvironment
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--algorithm', type=str, default='PPO',
+                    help='select one of algorithms among Vanilla_PG, NPG, TPRO, PPO')
+args = parser.parse_args()
+
+if args.algorithm == "PG":
+    from RL.vanila_pg import train_model
+elif args.algorithm == "NPG":
+    from RL.npg import train_model
+elif args.algorithm == "TRPO":
+    from RL.trpo import train_model
+elif args.algorithm == "PPO":
+    from RL.ppo import train_model
+
+if __name__ == "__main__":
+    target_text = 'This is an example text'
+    model_name = 'vicuna'
+    env = LLMEnvironment(model_name, target_text)
+    env.seed(500)
+    torch.manual_seed(500)
+
+    num_inputs = env.observation_space.shape[0]
+    num_actions = env.action_space.shape[0]
+
+    print('state size:', num_inputs)
+    print('action size:', num_actions)
+
+    actor = Actor(num_inputs, num_actions)
+    critic = Critic(num_inputs)
+
+    actor_optim = optim.Adam(actor.parameters(), lr=hp.actor_lr)
+    critic_optim = optim.Adam(critic.parameters(), lr=hp.critic_lr,
+                              weight_decay=hp.l2_rate)
+
+    running_state = ZFilter((num_inputs,), clip=5)
+    episodes = 0
+    xar = []
+    yar = []
+    for iter in range(50):
+        actor.eval(), critic.eval()
+        memory = deque()
+
+        steps = 0
+        scores = []
+        while steps < 2048:
+            episodes += 1
+            state = env.reset()
+            state = running_state(state)
+            score = 0
+            for _ in range(10000):
+
+                steps += 1
+                mu, std, _ = actor(torch.Tensor(state).unsqueeze(0))
+                action = get_action(mu, std)[0]
+                next_state, reward, done, _ = env.step(action)
+                next_state = running_state(next_state)
+
+                if done:
+                    mask = 0
+                else:
+                    mask = 1
+
+                memory.append([state, action, reward, mask])
+
+                score += reward
+                state = next_state
+
+                if done:
+                    break
+            scores.append(score)
+
+        score_avg = np.mean(scores)
+        print('{} episode score is {:.2f}'.format(episodes, score_avg))
+
+        with open('reward per iter.txt', 'w') as file:
+            file.write(str(episodes) + "," + str(score_avg))
+            file.write("\n")
+        file.close
+        xar.append(int(episodes))
+        yar.append(int(score_avg))
+
+        actor.train(), critic.train()
+        train_model(actor, critic, memory, actor_optim, critic_optim)
 
 
-model_name = 'vicuna' ### [vicuna, llama2, baichuan, internlm, chatglm, ziya]
-
-### 1. OoD Attack (initialized from random tokens)
-init_input = random_init(model_name, length=20)
-
-### 2. Weak Semantic Attack (initialized from the raw sentence)
-# init_input = 'Who was the victor of the United States presidential election in the year 2020?'
-
-target = 'Donald Trump was the victor of the United States presidential election in the year 2020.'
-mini_batch_size = 32 ### If CUDA out of memory, lower the mini_batch_size
-batch_size = 2048
-device = 'cuda:0'
-# steps = 768
-# topk = 256
-
-attacker_params = {
-    'update_strategy': 'gaussian',
-    'early_stop': True,
-    # 'is_save': True,
-    # 'save_dir': './result',
-}
+    def plotting():
+        plt.plot(xar, yar, linewidth=3)
+        plt.title("Avg score/Episodes", fontsize=19)
+        plt.xlabel("Episodes", fontsize=10)
+        plt.ylabel("Avg score", fontsize=10)
+        plt.tick_params(axis='both', labelsize=9)
+        plt.show()
 
 
-if __name__ == '__main__':
-    attacker = Attacker(
-        model_name,
-        init_input,
-        target,
-        device=device,
-        mini_batch_size=mini_batch_size,
-        batch_size=batch_size,
-        **attacker_params
-    )
-    attacker.run()
+    plotting()
+    print(xar, '\n', yar)
